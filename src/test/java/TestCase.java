63aafbf77edbc9380efba82bca12e1d114eef589
Naohide Sano <namachapanda2005@gmail.com>
2024-08-31 19:16:14 +0900

    add test case

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import libgme.VGMPlayer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import uk.co.omgdrv.simplevgm.VgmEmu;
+import vavi.util.Debug;
+import vavi.util.properties.annotation.Property;
+import vavi.util.properties.annotation.PropsEntity;
+
+
+/**
+ * TestCase.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
+ * @version 0.00 2024-08-30 nsano initial version <br>
+ */
+@PropsEntity(url = "file:local.properties")
+@EnabledIf("localPropertiesExists")
+class TestCase {
+
+    static boolean localPropertiesExists() {
+        return Files.exists(Paths.get("local.properties"));
+    }
+
+    @Property(name = "vavi.test.volume")
+    double volume = 0.2;
+
+    @Property
+    String vgz;
+
+    @BeforeEach
+    void setup() throws Exception {
+        if (localPropertiesExists()) {
+            PropsEntity.Util.bind(this);
+        }
+Debug.println("volume: " + volume);
+    }
+
+    @Test
+    void test1() throws Exception {
+        VGMPlayer player = new VGMPlayer(VgmEmu.VGM_SAMPLE_RATE_HZ);
+Debug.println(vgz);
+        player.setVolume(volume);
+        player.loadFile(vgz);
+        player.startTrack(1);
+        while (player.isPlaying()) Thread.yield(); // TODO add event system
+    }
+}
